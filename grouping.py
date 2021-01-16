61bad20efa6374d626903302f9997af2babc8c37
jelly-prog <76945483+jelly-prog@users.noreply.github.com>
2021-01-16 15:27:29 +0900

    Update grouping.py

--- grouping.py
+++ grouping.py
@@ -1,8 +1,8 @@
+import random
+
+import discord
 from discord.ext import commands
 from cerberus import Validator
-
-import random
-import discord
 
 class MakeTeam:
 
@@ -12,28 +12,19 @@ class MakeTeam:
         self.vc_state_err = '実行できません。ボイスチャンネルに入ってコマンドを実行してください。'
 
     def set_mem(self, ctx):
-        state = ctx.author.voice    # コマンド実行者のVCステータスを取得
-        if state is None:
+        state = ctx.author.voice # コマンド実行者のVCステータスを取得
+        if state is None: 
             return False
 
-        self.channel_mem = [i.name for i in state.channel.members]     # VCメンバリスト取得
-        self.mem_len = len(self.channel_mem)       # 人数取得
+        self.channel_mem = [i.name for i in state.channel.members] # VCメンバリスト取得
+        self.mem_len = len(self.channel_mem) # 人数取得
         return True
-
-    def teamtestcode(self, ctx):
-        state = ctx.author.voice   # コマンド実行者のVCステータスを取得
-        if state is None:
-            return False
-
-        self.channel_mem = [i.name for i in state.channel.members]   # VCメンバリスト取得
-        self.mem_len = len(self.channel_mem)    # 人数取得
-        return self.mem_len
 
     # チーム数を指定した場合のチーム分け
     def make_party_num(self, ctx, party_num, remainder_flag='false'):
         team = []
         remainder = []
-
+        
         if self.set_mem(ctx) is False:
             return self.vc_state_err
 
@@ -47,15 +38,15 @@ class MakeTeam:
         # チーム分けで余るメンバーを取得
         if remainder_flag:
             remainder_num = self.mem_len % party_num
-            if remainder_num != 0:
+            if remainder_num != 0: 
                 for r in range(remainder_num):
                     remainder.append(self.channel_mem.pop())
                 team.append("=====余り=====")
                 team.extend(remainder)
 
         # チーム分け
-        for i in range(party_num):
-            team.append("=====チーム" + str(i + 1) + "=====")
+        for i in range(party_num): 
+            team.append("=====チーム"+str(i+1)+"=====")
             team.extend(self.channel_mem[i:self.mem_len:party_num])
 
         return ('\n'.join(team))
@@ -80,15 +71,15 @@ class MakeTeam:
 
         # チーム分けで余るメンバーを取得
         remainder_num = self.mem_len % party_num
-        if remainder_num != 0:
+        if remainder_num != 0: 
             for r in range(remainder_num):
                 remainder.append(self.channel_mem.pop())
             team.append("=====余り=====")
             team.extend(remainder)
 
         # チーム分け
-        for i in range(party_num):
-            team.append("=====チーム" + str(i + 1) + "=====")
+        for i in range(party_num): 
+            team.append("=====チーム"+str(i+1)+"=====")
             team.extend(self.channel_mem[i:self.mem_len:party_num])
 
         return ('\n'.join(team))
