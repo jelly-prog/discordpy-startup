14a00e232395d92a63a77176ffbdaabf2100afb9
jelly-prog <76945483+jelly-prog@users.noreply.github.com>
2021-01-16 15:05:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,20 +1,18 @@
+from discord.ext import commands
 import os
 import traceback
 
-from discord.ext import commands
-# from grouping import MakeTeam
-
-# intents = discord.Intents.all()  # デフォルトのIntentsオブジェクトを生成
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+
 @bot.event
 async def on_command_error(ctx, error):
-orig_error = getattr(error, "original", error)
-error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-await ctx.send(error_msg)
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
-"""テストコマンド"""
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
